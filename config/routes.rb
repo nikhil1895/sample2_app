b037daeb0af7afd87ca774a1f8e6acc0a0fc2ec7
Nikhil <nikhil1895@gmail.com>
2016-01-29 22:27:39 +0530

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   
 
+  get 'sessions/new'
+
   root 'pages#home'
   get 'help' => 'pages#help'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' =>'sessions#destroy'
   resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
