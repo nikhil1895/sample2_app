e978ea6ea62453453dca043923c473e355b91f57
Nikhil <nikhil1895@gmail.com>
2016-02-01 01:45:40 +0530

    add account activations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
-
+  
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
@@ -30,6 +30,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  
-
   get 'sessions/new'
-
   root 'pages#home'
   get 'help' => 'pages#help'
   get 'about' => 'pages#about'
@@ -12,7 +9,7 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' =>'sessions#destroy'
   resources :users
-  
+  resources :account_activations, only: [:edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
